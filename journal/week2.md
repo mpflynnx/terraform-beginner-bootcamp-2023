92341b07cd7bef68a2ec92836b5b1e73a62eb5c4
Matthew Flynn <mpflynnx@gmail.com>
2023-10-10 09:07:10 +0000

    #41 Update server.rb Updated documentation

--- journal/week2.md
+++ journal/week2.md
@@ -1,13 +1,34 @@
 # <p align=center>Terraform Beginner Bootcamp 2023 Week 2
 
+## Week 2 Objectives.
+The objectives of week 2 where:
+- Clone a [Sinatra](https://en.wikipedia.org/wiki/Sinatra_%28software%29) web server.
+- Clone bash scripts used for CRUD.
+- Update Gitpod environment to automatically run the Terratowns Mock Server.
+
+
+
+
 <p align="center">
+
   <img src="../assets/week2.PNG"/>
+
 </p>
 
 # <p align=center>Week 2 Architecture Diagram </p>
 
 # Table of Contents
 
-- []()
-- []()
-- []()
+- [Sinartra](#sinatra)
+- [Gitpod environment](#gitpod-environment)
+
+
+## Sinatra 
+[Sinatra](https://sinatrarb.com/documentation.html) is a free and open source micro server framework written in Ruby.
+
+We have been provided with the code for this server. It will act as a mock server.
+
+We cloned the code from into this repository along with the Bash scripts for [Create Read Update Delete](https://en.wikipedia.org/wiki/Create,_read,_update_and_delete) (CRUD)
+
+## Gitpod environment
+We updated the gitpod.yml file to automatically start the server when a Gitpod workspace is launched.
--- terratowns_mock_server/server.rb
+++ terratowns_mock_server/server.rb
@@ -9,7 +9,14 @@ class Home
   include ActiveModel::Validations
   attr_accessor :town, :name, :description, :domain_name, :content_version
 
-  validates :town, presence: true
+  validates :town, presence: true, inclusion: { in: [
+    'melomaniac-mansion',
+    'cooker-cove',
+    'video-valley',
+    'the-nomad-pad',
+    'gamers-grotto'
+  ] }
+  # visible to all users
   validates :name, presence: true
   validates :description, presence: true
   validates :domain_name, 
@@ -79,7 +86,6 @@ class TerraTownsMockServer < Sinatra::Base
       halt 422, "Malformed JSON"
     end
 
-    # Validate payload data
     name = payload["name"]
     description = payload["description"]
     domain_name = payload["domain_name"]
@@ -148,7 +154,6 @@ class TerraTownsMockServer < Sinatra::Base
     # Validate payload data
     name = payload["name"]
     description = payload["description"]
-    domain_name = payload["domain_name"]
     content_version = payload["content_version"]
 
     unless params[:uuid] == $home[:uuid]
@@ -157,9 +162,9 @@ class TerraTownsMockServer < Sinatra::Base
 
     home = Home.new
     home.town = $home[:town]
+    home.domain_name = $home[:domain_name]
     home.name = name
     home.description = description
-    home.domain_name = domain_name
     home.content_version = content_version
 
     unless home.valid?
@@ -181,7 +186,7 @@ class TerraTownsMockServer < Sinatra::Base
     end
 
     $home = {}
-    { message: "House deleted successfully" }.to_json
+    { uuid: uuid }.to_json
   end
 end
 
