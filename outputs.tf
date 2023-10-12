8c95d0a4833db3cfa4b110421579f1273150f0af
Matthew Flynn <mpflynnx@gmail.com>
2023-10-12 14:27:40 +0000

    #53 Refactored public folder Others items completed as per issue 53

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,29 @@
-output "s3_bucket_name" {
+output "home_frontier_hosting_s3_bucket_name" {
   description = "S3 bucket name"
-  value = module.terrahome_aws.bucket_name
+  value = module.home_frontier_hosting.bucket_name
 }
 
-output "s3_website_endpoint" {
+output "home_frontier_hosting_s3_website_endpoint" {
   description = "S3 Static Website hosting endpoint"
-  value = module.terrahome_aws.website_endpoint
+  value = module.home_frontier_hosting.website_endpoint
 }
 
-output "cloudfront_url" {
+output "home_frontier_hosting_cloudfront_url" {
   description = "CloudFront Distribution domain name"
-  value = module.terrahome_aws.cloudfront_url
+  value = module.home_frontier_hosting.domain_name
+}
+
+output "home_okcomputer_hosting_s3_bucket_name" {
+  description = "S3 bucket name"
+  value = module.home_okcomputer_hosting.bucket_name
+}
+
+output "home_okcomputer_hosting_s3_website_endpoint" {
+  description = "S3 Static Website hosting endpoint"
+  value = module.home_okcomputer_hosting.website_endpoint
+}
+
+output "home_okcomputer_hosting_cloudfront_url" {
+  description = "CloudFront Distribution domain name"
+  value = module.home_okcomputer_hosting.domain_name
 }
